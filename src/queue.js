cb2ba2807fc173ec25d588b6e772578da8ee6615
mikegrekov <svg200914@gmail.com>
2023-12-21 19:25:03 +0000

    deploy

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    //  this._queue = {};
+    this.first = null;
+    this.last = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+      }
+    return this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.first) return null;
+    let removed = this.first.value;
+    if (this.first === this.last) this.last = null;
+    this.first = this.first.next;
+    this.length--;
+    return removed;
   }
 }
 
